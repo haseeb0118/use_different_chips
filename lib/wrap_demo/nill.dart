050776f3466803a06db875ace1bc50ba0aa13d6c
haseeb0118 <haseeburrahman47@gmail.com>
2021-12-23 09:11:57 +0500

    nill_changes

---
+++ lib/wrap_demo/nill.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class NillPage extends StatefulWidget {
+  const NillPage({Key? key}) : super(key: key);
+
+  @override
+  _NillPageState createState() => _NillPageState();
+}
+
+class _NillPageState extends State<NillPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
